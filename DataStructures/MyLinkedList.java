65f438d7f2f81718f47316e4247cf76ecfd98fec
Gitosthenes <abledso3@uw.edu>
2020-05-11 13:24:15 -0500

    Update LL Implementation

--- DataStructures/MyLinkedList.java
+++ DataStructures/MyLinkedList.java
@@ -1,6 +1,8 @@
+package DataStructures;
+
 class MyLinkedList<T> {
-    private Node<T> mHead;
-    private Node<T> mTail;
+    private Node mHead;
+    private Node mTail;
     private int mSize;
 
     public MyLinkedList() {
@@ -9,68 +11,58 @@ class MyLinkedList<T> {
         mSize = 0;
     }
 
-    public void insert(final T tData) {
-        if(tData == null) { return; }
-
-        Node<T> newNode = new Node<>(tData);
+    public void insertHead(final T tData) {
+        Node newNode = new Node(tData);
         if(mSize == 0) {
             mHead = mTail = newNode;
         } else {
-            Node<T> oldTail = mTail;
-            mTail = newNode;
-            oldTail.setNext(newNode);
-            newNode.setPrev(oldTail);
+            newNode.setNext(mHead);
+            mHead = newNode;
         }
         mSize++;
     }
 
-    public void delete(final T tData) {
-        if(mSize == 0 || tData == null) { return; }
-
-        Node<T> currNode = mHead;
-        while(currNode.getNext() != null) {
-            if(tData.equals(currNode.getData())) {
-                if(currNode == mHead) { //elem to delete is head
-                    mHead = currNode.getNext();
-                } else {// elem to delete is in middle
-                    Node<T> left = currNode.getPrev();
-                    Node<T> right = currNode.getNext();
-                    left.setNext(right);
-                    right.setPrev(left);
-                }
-                mSize--;
-                return;
-            }
-            currNode = currNode.getNext();
+    public void insertTail(final T tData) {
+        if(mSize == 0) {
+            mHead = mTail = new Node(tData);
+        } else {
+            mTail.setNext(new Node(tData));
         }
+        mSize++;
+    }
 
-        if(tData.equals(currNode.getData())) {// elem to delete is tail
-            mTail = currNode.getPrev();
-            mSize--;
-        }
+    public void removeHead() { 
+        mHead = mHead.getNext();
+        mHead.setPrev(null);
+        mSize--;
+    }
+
+    public void removeTail() {
+        mTail = mTail.getPrev();
+        mTail.setNext(null);
+        mSize--;
     }
 
     public T getHead() { return mHead.getData(); }
     public T getTail() { return mTail.getData(); }
     public int size() { return mSize; }
-}
 
-class Node<T> {
-    private T mData;
-    private Node<T> mPrev;
-    private Node<T> mNext;
+    private class Node {
+        private T mData;
+        private Node mPrev;
+        private Node mNext;
 
-    public Node(final T tData) {
-        mData = tData;
-        mPrev = null;
-        mNext = null;
+        public Node(final T tData) {
+            mData = tData;
+            mPrev = null;
+            mNext = null;
+        }
+
+        public T getData() { return mData; }
+        public Node getPrev() { return mPrev; }
+        public Node getNext() { return mNext; }
+
+        public void setPrev(final Node tPrev) { mPrev = tPrev; }
+        public void setNext(final Node tNext) { mNext = tNext; }
     }
-
-    public T getData() { return mData; }
-    public Node<T> getPrev() { return mPrev; }
-    public Node<T> getNext() { return mNext; }
-
-    public void setData(final T tData) { mData = tData; }
-    public void setPrev(final Node<T> tPrev) { mPrev = tPrev; }
-    public void setNext(final Node<T> tNext) { mNext = tNext; }
 }
